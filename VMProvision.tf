417b99145921f1189df597621043d2c9ae94284f
Babu <babu.rengaraju@gmail.com>
2020-01-08 10:09:22 +0530

    Add files via upload

---
+++ VMProvision.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAWCCS5BFRDTLEEYHE"
+  secret_key = "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"
+}
+
+  data "aws_ami" "server_ami" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+  name = "name"
+  values = ["amzn-ami-hvm*-x86_64-gp2"]
+}
+}
+  resource "aws_instance" "web" {
+  ami           = data.aws_ami.server_ami.id
+  instance_type = "t2.micro"
+  tags = {
+  Name = "New VM Provision"
+}
+}
